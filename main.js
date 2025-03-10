dbe9a1687dc0d94321ce17f0c0457da9895d0ecc
unknown <minh.trancong@NSK.LOCAL>
2025-03-10 15:27:16 +0900

    add animation

--- main.js
+++ main.js
@@ -57,34 +57,111 @@ const HeaderModule = (function () {
     });
 })();
 
-var swiper = new Swiper(".background", {
-    loop: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    effect: "fade",
+// var swiper = new Swiper(".background", {
+//     loop: true,
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     effect: "fade",
+// });
+
+CustomEase.create("hop", "M0,0 C0.355,0.022 0.448,0.079 0.5,0.5 0.542,0.846 0.615,1 1,1 ");
+CustomEase.create("hop2", "M0,0 C0.078,0.617 0.114,0.716 0.255,0.828 0.373,0.922 0.561,1 1,1 ");
+
+const mainTl = gsap.timeline();
+const revealerTl = gsap.timeline();
+const scaleTl = gsap.timeline();
+
+revealerTl.to(".curtain-top", {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+    duration: 1.5,
+    ease: "hop"
+}).to(".curtain-bottom", {
+    clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+    duration: 1.5,
+    ease: "hop"
+}, "<");
+
+revealerTl.to(".r-1", {
+    clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+    duration: 1.5,
+    ease: "hop"
+}).to(".r-2", {
+    clipPath: "polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)",
+    duration: 1.5,
+    ease: "hop"
+}, "<");
+
+scaleTl.to(" .intro_img:first-child", {
+    scale: 1,
+    duration: 2,
+    ease: "power4.inOut"
 });
 
-swiper.on('slideChange', function () {
-    const navigationtitle = document.querySelector('.js-navigation_title');
-    navigationtitle.textContent = `0${swiper.realIndex + 1}`
-});
+const images = document.querySelectorAll(".intro_img:not(:first-child)");
 
-    options = {
-	"cursorOuter": "circle",
-	"hoverEffect": "circle-move",
-	"hoverItemMove": false,
-	"defaultCursor": false,
-	"outerWidth": 30,
-	"outerHeight": 30
-      };
-    magicMouse(options);
+images.forEach((img, index) => {
+    scaleTl.to(img, {
+        display: "block",
+        scale: 1,
+        duration: 1,
+        ease: "power3.out"
+    },
+        ">-0.5"
+    )
+})
+
+mainTl
+    .add(revealerTl)
+    .add(scaleTl, "-=1.25")
+    .add(() => {
+        document
+            .querySelectorAll(".img:not(.main)")
+            .forEach((img) => img.remove());
+
+        const state = Flip.getState(".main");
+        const imagesContainer = document.querySelector(".images");
+
+        imagesContainer.classList.add("stacked-container");
+
+        document.querySelectorAll(".main").forEach((img, i) => {
+            img.classList.add("stacked");
+            img.style.order = i;
+            gsap.set(".img.stacked", {
+                clearProps:
+                    "transform, top, left"
+            });
+        });
+
+        return Flip.from(state, {
+            duration: 2,
+            ease: "hop",
+            absolute: true,
+            stagger: {
+                amount: -0.3,
+            }
+        });
+    })
+
+// swiper.on('slideChange', function () {
+//     const navigationtitle = document.querySelector('.js-navigation_title');
+//     navigationtitle.textContent = `0${swiper.realIndex + 1}`
+// });
+
+options = {
+    "hoverEffect": "circle-move",
+    "hoverItemMove": false,
+    "defaultCursor": false,
+    "outerWidth": 30,
+    "outerHeight": 30
+};
+magicMouse(options);
